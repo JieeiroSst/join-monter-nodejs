1a5fcde4042958b9d5860e13a3eaac739e471100
Alan luu <luumanhquan.91@gmail.com>
2020-10-09 16:44:21 +0700

    fix

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mount = require('koa-mount');
 const bodyParser = require('koa-body-parser');
 
 const schema = require('./graphql');
-const schemaTest = require('./graphql/resolvers/test');
 const loginRouter = require('./api/login');
 
 const app = new Koa();
